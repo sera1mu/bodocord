1bbff4f5a38123f93c77987fae369598c556a24b
Seraimu <seraimu.neko@outlook.jp>
2021-12-12 11:33:25 +0900

    Add BodocordClient

---
+++ src/structures/BodocordClient.ts
@@ -0,0 +1,24 @@
+import {
+  Client,
+  ClientOptions,
+  event
+} from 'harmony';
+
+import {
+  Logger
+} from 'std/log';
+
+export class BodocordClient extends Client {
+  logger: Logger;
+
+  constructor(logger: Logger, options?: ClientOptions) {
+    super(options);
+
+    this.logger = logger;
+  }
+
+  @event()
+  ready() : void{
+    this.logger.info(`Ready! Logged in as ${this.user?.tag}(${this.user?.id})`);
+  }
+}
