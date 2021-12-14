4ed0b130337063659cc5e4cfb3f851ccb9842446
Seraimu <seraimu.neko@outlook.jp>
2021-12-14 17:50:53 +0900

    Register command at ready in BodocordClient

--- src/structures/BodocordClient.ts
+++ src/structures/BodocordClient.ts
@@ -1,10 +1,13 @@
 import { Client, ClientOptions, event } from "harmony";
 import pino from "pino";
+import Command from "./Command.ts";
 
 /**
  * Bodocord Discord gateway client
  */
 export default class BodocordClient extends Client {
+  readonly commands: Command[];
+
   private readonly logger: pino.Logger;
 
   /**
@@ -12,16 +15,28 @@ export default class BodocordClient extends Client {
    * @param options Client options
    */
   constructor(
+    commands: Command[],
     loggerOptionsOrStream?: pino.LoggerOptions | pino.DestinationStream,
     options?: ClientOptions,
   ) {
     super(options);
 
+    this.commands = commands;
     this.logger = pino(loggerOptionsOrStream);
   }
 
   @event()
   ready(): void {
+    this.logger.info("Registering commands...");
+
+    // Register commands
+    this.commands.forEach(async (command) => {
+      await this.interactions.commands.create(command.commandPartial);
+      this.logger.info(`Registered command ${command.commandPartial.name}`);
+    });
+
+    this.logger.info("All commands registered.");
+
     this.logger.info(`Ready! Logged in as ${this.user?.tag}(${this.user?.id})`);
   }
 }
