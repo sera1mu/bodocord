6a13d13e736e9622ea37d27a30af6936a23c1013
Seraimu <seraimu.neko@outlook.jp>
2021-12-14 16:30:48 +0900

    Add gracefully shutdown

--- src/boot.ts
+++ src/boot.ts
@@ -5,9 +5,9 @@ import { getConfig } from "./util/configUtil.ts";
 
 /**
  * Bodocord necessary environment variables
- * 
+ *
  * BC_CONFIG -> Config file path
- * 
+ *
  * BC_TOKEN -> Client token
  */
 interface EnvironmentVariables {
@@ -47,6 +47,29 @@ const getEnv =
   };
 
 /**
+ * Shutdown bot gracefully
+ * @param client 
+ * @param logger 
+ */
+const shutdown = async function gracefullyShutdownBot(
+  client: BodocordClient,
+  logger: pino.Logger,
+) {
+  logger.info("Shutting down...");
+
+  try {
+    // Destroy client
+    await client.destroy();
+  } catch (err) {
+    logger.error(err, "Failed to destroy client gracefully.");
+    logger.info("Exit code is 1.");
+    Deno.exit(1);
+  }
+
+  logger.info("Exit code is 0.");
+};
+
+/**
  * Boot the bot
  * @returns System logger
  */
@@ -58,33 +81,36 @@ const boot = async function bootBot() {
   const config = getConfig(BC_CONFIG);
 
   // Create logger
-  const logger = pino(config.loggers['system']);
+  const logger = pino(config.loggers["system"]);
 
   // Create client
-  const client = new BodocordClient(config.loggers['client']);
+  const client = new BodocordClient(config.loggers["client"]);
 
   // Connect gateway
   await client.connect(BC_TOKEN, Intents.None);
 
-  return { logger };
+  // Add signal listeners
+  Deno.addSignalListener("SIGTERM", () => shutdown(client, logger));
+  Deno.addSignalListener("SIGINT", () => shutdown(client, logger));
+
+  return { client, config, logger };
 };
 
 // Measure boot time
 performance.mark("bootStart");
-const { logger } = await boot();
+const { client, config, logger } = await boot();
 performance.mark("bootEnd");
 
 performance.measure(
   "boot",
   "bootStart",
-  "bootEnd"
+  "bootEnd",
 );
 
 // Get boot time result
-const bootTimeResult = performance.getEntriesByName("boot")[0]
+const bootTimeResult = performance.getEntriesByName("boot")[0];
 const divisionMiliSeconds = 1000;
 
 // Log completed
 logger.info(`Done(${bootTimeResult.duration / divisionMiliSeconds} s)!`);
 logger.debug(bootTimeResult, "Boot measure result");
-
