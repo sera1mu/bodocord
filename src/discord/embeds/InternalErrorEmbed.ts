e2fb329e5f762c4d3dfae09e6c26dc2746f07bea
Seraimu <seraimu.neko@outlook.jp>
2021-12-17 20:48:38 +0900

    Set the message as a template sentence in InternalError
    変えたい場合は直接 this.description = ... してあげてください

--- src/discord/embeds/InternalErrorEmbed.ts
+++ src/discord/embeds/InternalErrorEmbed.ts
@@ -8,7 +8,11 @@ export default class InternalErrorEmbed extends ErrorEmbed {
    * @param message Error message
    * @param hash Error Hash (MD5)
    */
-  constructor(message: string, hash: string) {
+  constructor(hash: string) {
+    const message = `An error has occurred within the server.
+    Please try again in a few minutes.
+    If you get this error again and again, please contact the administrator with the hash value in the footer.`;
+
     super(message, hash);
 
     this.title = "Internal Error";
